c1c8e2ad7a272f50d1d8320e0d0cf94ace7a53d7
Ian Boyle <irboyle21@gmail.com>
2023-04-16 11:52:01 -0400

    adds get user fetch

--- src/store/index.ts
+++ src/store/index.ts
@@ -2,6 +2,7 @@ import { configureStore} from '@reduxjs/toolkit'
 import { IPosition } from '../models/position.model';
 
 import { positionsApi, useFetchPositionsQuery } from './apis/positionsApi';
+import { userApi, useFetchUserQuery } from './apis/userApi';
 import { setupListeners } from '@reduxjs/toolkit/dist/query';
 
 export interface IPositionsState {
@@ -9,15 +10,17 @@ export interface IPositionsState {
 }
 const store = configureStore({
   reducer: {
-    [positionsApi.reducerPath]: positionsApi.reducer
+    [positionsApi.reducerPath]: positionsApi.reducer,
+    [userApi.reducerPath]: userApi.reducer
   },
   middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(positionsApi.middleware),
+    getDefaultMiddleware().concat(positionsApi.middleware).concat(userApi.middleware)
 
 });
 
 setupListeners(store.dispatch)
 export {
   store,
-  useFetchPositionsQuery
+  useFetchPositionsQuery,
+  useFetchUserQuery
 }
