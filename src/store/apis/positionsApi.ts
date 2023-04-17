b9f5a2e3faf8b86d0cc73c514c1c9751314d59ad
Ian Boyle <irboyle21@gmail.com>
2023-04-17 12:01:24 -0400

    adds pause

--- src/store/apis/positionsApi.ts
+++ src/store/apis/positionsApi.ts
@@ -1,12 +1,21 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 import { IPosition } from '../../models/position.model';
 
-
+const pause = (duration: number) => {
+  return new Promise((resolve) => {
+    setTimeout(resolve, duration)
+  })
+}
 const positionsApi = createApi({
   reducerPath: 'positions',
   tagTypes: ['Positions'],
   baseQuery: fetchBaseQuery({ 
     baseUrl: `${process.env.REACT_APP_API_URL}`,
+    //REMOVE FOR PROD
+    fetchFn: async (...args) => {
+      await pause(1000)
+      return fetch(...args)
+    },
     prepareHeaders: (headers, { getState }) => {
       const token = localStorage.getItem('token')?.replace(/['"]+/g, '')
       if (token) {
