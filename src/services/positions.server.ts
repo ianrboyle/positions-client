2827f616046d94b9ae749d5d99b522fc77aef55c
Ian Boyle <irboyle21@gmail.com>
2023-05-16 22:16:50 -0400

    adds ticker search

--- src/services/positions.server.ts
+++ src/services/positions.server.ts
@@ -53,4 +53,43 @@ export const getPositionsFetch = async ():Promise<IPosition[]> => {
   })
   return response.json();
 }
+export const tickerSearch = async (searchText: string): Promise<TickerSearch | undefined> => {
+  
+  // const token = localStorage.getItem('token') || ""
+  try {
 
+    const response = await fetch((`https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords=${searchText}&apikey=N8HHW6H9CU7C2Q6B`),{
+      method: "GET",
+      headers: {
+        'User-Agent': 'request',
+      },
+    });
+    if (!response.ok) {
+      console.log('Status:', response.status);
+    } else {
+      const data: TickerSearch = await response.json();
+      return data;
+    }
+  } catch (error) {
+    console.log('Error:', error);
+    return;
+  }
+
+
+}
+
+export interface TickerSearch {
+  bestMatches: BestMatch[];
+}
+
+export interface BestMatch {
+  "1. symbol":      string;
+  "2. name":        string;
+  "3. type":        string;
+  "4. region":      string;
+  "5. marketOpen":  string;
+  "6. marketClose": string;
+  "7. timezone":    string;
+  "8. currency":    string;
+  "9. matchScore":  string;
+}
